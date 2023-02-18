bc1cc31a3b31f5c70a04fd2d1acf5a1d8897eb4c
Christian Klauser <christianklauser@outlook.com>
2023-02-18 15:05:59 +0100

    Don't assert equivalence of PointsVec.Normalized

---
+++ Splitracker.Persistence.Test/FluentAssertionSettings.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
+using Splitracker.Domain;
+
+namespace Splitracker.Persistence.Test;
+
+[SuppressMessage("Usage", "CA2255:The \'ModuleInitializer\' attribute should not be used in libraries",
+    Justification = "Tests are basically an application.")]
+static class FluentAssertionSettings
+{
+    [ModuleInitializer]
+    public static void Configure()
+    {
+        AssertionOptions.AssertEquivalencyUsing(opts =>
+            // It never makes sense to assert equivalence of PointsVec.Normalized. It's a pure function.
+            opts.Excluding(m =>
+                m.DeclaringType == typeof(PointsVec) && m.Name == nameof(PointsVec.Normalized)
+                )
+            );
+    }
+}
