4e39f1bcf3406ddb27489fcf73e3b3f634f5bddd
Christian Klauser <christianklauser@outlook.com>
2023-02-25 23:33:27 +0100

    Fixed default point types for LE/FO (V/E)

--- Splitracker.Domain/Commands/PoolType.cs
+++ Splitracker.Domain/Commands/PoolType.cs
@@ -1,7 +1,19 @@
-﻿namespace Splitracker.Domain.Commands;
+﻿using System;
+using System.Diagnostics;
+
+namespace Splitracker.Domain.Commands;
 
 public enum PoolType
 {
     Fo,
-    Lp
+    Lp,
+}
+
+public static class PoolTypeExtensions
+{
+    public static PointType DefaultPointType(this PoolType type) => type switch {
+        PoolType.Fo => PointType.E,
+        PoolType.Lp => PointType.V,
+        _ => throw new ArgumentOutOfRangeException(nameof(type), type, "Unknown pool type"),
+    };
 }
