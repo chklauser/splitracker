98972fe8b852f980fa055b3ab71ff45a8a3aa6f6
Christian Klauser <christianklauser@outlook.com>
2023-06-08 22:33:11 +0200

    Deploy to Hetzner

--- Klauser.Do.Infrastructure/download-admin-cert.ps1
+++
@@ -1,2 +0,0 @@
-kubectl get secret -nraven raven-admin-cert -ojsonpath="{.data['keystore\.p12']}" | convertfrom-base64 -AsByteArray | set-content -AsByteStream admin.pfx
-kubectl get secret -nraven raven-admin-cert-password-ee75a3df -ojsonpath="{.data.password}" | Convertfrom-base64
