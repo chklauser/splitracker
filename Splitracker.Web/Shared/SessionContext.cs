b17561b183642aafa1590a3ec8a9d8c48f4b71f8
Christian Klauser <christianklauser@outlook.com>
2023-12-02 21:51:58 +0100

    character action dialog

--- Splitracker.Web/Shared/SessionContext.cs
+++ Splitracker.Web/Shared/SessionContext.cs
@@ -5,4 +5,6 @@ namespace Splitracker.Web.Shared;
 public record SessionContext
 {
     public IImmutableList<string> FilterTags { get; set; } = ImmutableList<string>.Empty;
+    
+    public int RollTarget { get; set; }
 }
